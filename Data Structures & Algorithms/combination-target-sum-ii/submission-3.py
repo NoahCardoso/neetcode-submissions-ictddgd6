8be795ae0aa04fe13e28de4938ce1d1159e3f8cc
Noah Cardoso <106560551+NoahCardoso@users.noreply.github.com>
2026-04-04 17:39:03 -0400

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.py
@@ -0,0 +1,26 @@
+import copy
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        paths = []
+        nums = sorted(candidates)
+        
+        def permute(path, total, index):
+            if total > target:
+                return 
+            if total == target:
+                paths.append(path.copy())
+                return
+            
+            for i in range(index, len(nums)):
+                if total + nums[i] > target:
+                    break
+                if i > index and nums[i] == nums[i - 1]:
+                    continue
+                path.append(nums[i])
+                permute(path,total+nums[i], i+1)
+                path.pop()
+                
+        
+        permute([],0,0)
+
+        return paths
